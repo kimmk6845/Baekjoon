72824212862dd14f4821d752e85da820d80755b7
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2021-12-19 22:43:17 +0900

    Add files via upload

---
+++ Sorting/10989(정렬_카운팅정렬).cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#define MAX_SIZE 10001
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int arr[MAX_SIZE] = { 0 };
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr[num]++;
+    }
+    for (int i = 1; i < MAX_SIZE; i++)
+        for (int j = 0; j < arr[i]; j++)
+            cout << i << "\n";
+}
+
+// 카운팅 정렬(계수 정렬)
+
+/*
+
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 
+둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+
+
+예제 입력 1
+10
+5
+2
+3
+1
+4
+2
+3
+5
+1
+7
+
+예제 출력 1
+1
+1
+2
+2
+3
+3
+4
+5
+5
+7
+
+*/
